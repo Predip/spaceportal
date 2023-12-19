af61209aa6102c3569868d4c14f2863b545b26b0
Predip <piradip_94@hotmail.com>
2023-12-19 13:29:04 +0100

    add host

--- spaceportal/settings.py
+++ spaceportal/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-5478g3##4cik+3^3^b-n6p3%0$r7bps@-@)4giymh*-*$9wh!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.87.255.187']
 
 # Application definition
 
