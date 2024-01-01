0a876e67175debbf5623e65ccfb46283e30794df
Predip <piradip_94@hotmail.com>
2024-01-01 23:12:44 +0100

    add multiple asteroids
    - add basic filter logic
    -- for is_potentially_hazardous & is_sentry_object
    - reformat asteroid.js
    -- Make the earth transparent
    -- calculate the distance to the earth more dynamic
    -- not clickable if the filter is open

--- spacetravel/static/asteroid.js
+++ spacetravel/static/asteroid.js
@@ -4,106 +4,122 @@ import * as THREE from 'https://threejs.org/build/three.module.js';
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
+let factor = null;
+let asteroids = null;
+
 // Set up scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5);
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer({
+  alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 sceneContainer.appendChild(renderer.domElement);
 
 // Create Earth
 const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
 const earthTexture = new THREE.TextureLoader().load('../static/earth_daymap.jpg');
-const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
+const earthMaterial = new THREE.MeshBasicMaterial({
+  map: earthTexture,
+  transparent: true,
+  opacity: 0.6
+});
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earth);
 
 // Create Asteroid Style
 const asteroidTexture = new THREE.TextureLoader().load('../static/Comet_from_331_m.png');
-const asteroidMaterial = new THREE.MeshBasicMaterial({ map: asteroidTexture, transparent: true, opacity: 1, depthWrite: true });
+const asteroidMaterial = new THREE.MeshBasicMaterial({
+  map: asteroidTexture,
+  transparent: true,
+  opacity: 1,
+  depthWrite: true
+});
 const asteroidsMesh = [];
-let asteroids = null;
 
 
 // Animation
 const animate = () => {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    // Rotate Earth
-    earth.rotation.y += 0.005;
+  // Rotate Earth
+  earth.rotation.y += 0.005;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 // Handle window resize
 window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 // Handle mouse click
 window.addEventListener('click', (event) => {
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  if (document.getElementById("filterForm").style.display == "none") {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  raycaster.setFromCamera(mouse, camera);
+    raycaster.setFromCamera(mouse, camera);
 
-  // Store the closest asteroid and its distance
-  let closestAsteroid = null;
-  let closestDistance = Infinity;
-  let closestAsteroidIndex = -1
+    // Store the closest asteroid and its distance
+    let closestAsteroid = null;
+    let closestDistance = Infinity;
+    let closestAsteroidIndex = -1
 
-  asteroidsMesh.forEach((asteroid, index) => {
-    const closestPoint = new THREE.Vector3();
-    raycaster.ray.closestPointToPoint(asteroid.position, closestPoint);
+    asteroidsMesh.forEach((asteroid, index) => {
+      const closestPoint = new THREE.Vector3();
+      raycaster.ray.closestPointToPoint(asteroid.position, closestPoint);
 
-    // Calculate distance to the asteroid
-    const distanceToAsteroid = closestPoint.distanceTo(asteroid.position);
+      // Calculate distance to the asteroid
+      const distanceToAsteroid = closestPoint.distanceTo(asteroid.position);
 
-    // Check if the distance is less than the asteroid's radius
-    if (distanceToAsteroid < asteroid.geometry.parameters.radius && distanceToAsteroid < closestDistance) {
-      closestAsteroid = asteroid;
-      closestAsteroidIndex = index;
-      closestDistance = distanceToAsteroid;
+      // Check if the distance is less than the asteroid's radius
+      if (distanceToAsteroid < asteroid.geometry.parameters.radius && distanceToAsteroid < closestDistance) {
+        closestAsteroid = asteroid;
+        closestAsteroidIndex = index;
+        closestDistance = distanceToAsteroid;
+      }
+    });
+
+    // If an asteroid is clicked, display its information
+    if (closestAsteroid) {
+      const popupText = `Asteroid Information\n
+        Name: ${asteroids[closestAsteroidIndex].name}\n
+        Distance: ${closestDistance.toFixed(2)*factor} km`;
+      alert(popupText);
     }
-  });
-
-  // If an asteroid is clicked, display its information
-  if (closestAsteroid) {
-    const popupText = `Asteroid Information\n
-    Name: ${asteroids[closestAsteroidIndex].name}\n
-    Distance: ${closestDistance.toFixed(2)*10000000} km`;
-    alert(popupText);
   }
 });
 
-// Function to import data into the module
-export function importAsteroidModule(asteroidsData) {
-    asteroids = asteroidsData;
-    console.log('Asteroids data:', asteroids);
-
-    // For example, you can access specific fields
-    asteroids.forEach(asteroid => {
-        //console.log('Asteroid Name:', asteroidData.name);
-        // Add your logic here to work with the data and create asteroids
-        const newAsteroid = createAsteroid(asteroid);
-        asteroidsMesh.push(newAsteroid)
-        scene.add(newAsteroid);
-    });
-}
-
 // Function to create an asteroid
 function createAsteroid(asteroidData) {
-    //console.log(asteroidData)
-    const asteroidGeometry = new THREE.SphereGeometry(0.1, 16, 16);
-    const asteroid = new THREE.Mesh(asteroidGeometry, asteroidMaterial.clone());
-    //const asteroidDistance = 5; // Distance from Earth
-    const position = asteroidData.position.map(coord => coord * 0.00000001)
-    asteroid.position.set(position[0], position[1], position[2]);
-    return asteroid;
+  //console.log(asteroidData)
+  const asteroidGeometry = new THREE.SphereGeometry(0.1, 16, 16);
+  const asteroid = new THREE.Mesh(asteroidGeometry, asteroidMaterial.clone());
+  //const asteroidDistance = 5; // Distance from Earth
+  const position = asteroidData.position.map(coord => coord * ((1 / factor)))
+  asteroid.position.set(position[0], position[1], position[2]);
+  return asteroid;
 }
 
 // Start animation
 animate();
+
+
+// Function to import data into the module
+export function importAsteroidModule(asteroidsData, newFactor) {
+  asteroids = asteroidsData;
+  factor = newFactor;
+  //console.log('Asteroids data:', asteroids);
+  //console.log('Factor:', factor);
+
+  asteroids.forEach(asteroid => {
+    //console.log('Asteroid Name:', asteroidData.name);
+    const newAsteroid = createAsteroid(asteroid);
+    asteroidsMesh.push(newAsteroid)
+    scene.add(newAsteroid);
+  });
+}
--- spacetravel/views.py
+++ spacetravel/views.py
@@ -3,6 +3,7 @@ from .functions.asteroids.orbit_position import calculate_current_orbit
 from .functions.wordcloud_generator import WordCloudGenerator
 from spacetravel.models.neo import FactSheet
 from spacetravel.models.news import News
+from django.db.models import Q
 from django.shortcuts import render
 
 
@@ -15,14 +16,72 @@ def space_info(request):
 
 
 def asteroids_explorer(request):
-    asteroids = FactSheet.objects.using('neo').all()
+    # Get filter parameters from the request
+    is_potentially_hazardous = request.GET.get('is_potentially_hazardous', False)
+    is_sentry_object = request.GET.get('is_sentry_object', False)
+    diameter = request.GET.get('diameter', None)
+    type_id = request.GET.get('type_id', None)
+    magnitude = request.GET.get('magnitude', None)
+    year = request.GET.get('year', None)
+    quarter = request.GET.get('quarter', None)
+    month = request.GET.get('month', None)
+    week = request.GET.get('week', None)
+    day = request.GET.get('day', None)
+    hour = request.GET.get('hour', None)
+    minute = request.GET.get('minute', None)
+
+    # Convert string values to boolean
+    is_potentially_hazardous = is_potentially_hazardous.lower() == 'true' if is_potentially_hazardous else False
+    is_sentry_object = is_sentry_object.lower() == 'true' if is_sentry_object else False
+
+    # Build the query based on the filter parameters
+    filters = Q()
+    filters &= Q(asteroid_id__is_potentially_hazardous=is_potentially_hazardous)
+    filters &= Q(asteroid_id__is_sentry_object=is_sentry_object)
+    if diameter is not None:
+        filters &= Q(diameter_id__kilometer=diameter)
+    if type_id is not None:
+        filters &= Q(type_id=type_id)
+    if magnitude is not None:
+        filters &= Q(magnitude_id__magnitude=magnitude)
+    if year is not None:
+        filters &= Q(time_id__year=year)
+    if quarter is not None:
+        filters &= Q(time_id__quarter=quarter)
+    if month is not None:
+        filters &= Q(time_id__month=month)
+    if week is not None:
+        filters &= Q(time_id__week=week)
+    if day is not None:
+        filters &= Q(time_id__day=day)
+    if hour is not None:
+        filters &= Q(time_id__hour=hour)
+    if minute is not None:
+        filters &= Q(time_id__minute=minute)
+
+    asteroids = FactSheet.objects.using('neo').filter(filters)
+
+    # Calculate max and min distances
+    min_distance = float('inf')
+
+    for asteroid in asteroids:
+        position = calculate_current_orbit(asteroid.asteroid_id)
+        distance = ((position[0] ** 2 + position[1] ** 2 + position[2] ** 2) ** 0.5).value
+
+        if distance < min_distance:
+            min_distance = distance
 
     asteroids_data = [
-        {'name': asteroid.name, 'position': tuple(q.value for q in calculate_current_orbit(asteroid.asteroid_id))}
+        {
+            'name': asteroid.name,
+            'position': tuple(q.value for q in calculate_current_orbit(asteroid.asteroid_id))
+        }
         for asteroid in asteroids
     ]
 
-    return render(request, 'asteroids_explorer.html', {'asteroids': json.dumps(asteroids_data)})
+    return render(request, 'asteroids_explorer.html', {'asteroids': json.dumps(asteroids_data),
+                                                       'factor': min_distance
+                                                       })
 
 
 def weather(request):
@@ -38,4 +97,3 @@ def news_collection(request):
 def news_details(request, news_id):
     news = News.objects.get(id=news_id)
     return render(request, 'news/details.html', {'news': news})
-
